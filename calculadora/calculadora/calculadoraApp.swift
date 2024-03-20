aabde198097f77be1fdaadd0e7d2f636dd4ecc58
Rubén <ruben.sr.apple@gmail.com>
2024-03-20 11:36:49 +0100

    Iniciando proyecto en Xcode

---
+++ calculadora/calculadora/calculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculadoraApp.swift
+//  calculadora
+//
+//  Created by Ruben on 20/3/24.
+//
+
+import SwiftUI
+
+@main
+struct calculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
